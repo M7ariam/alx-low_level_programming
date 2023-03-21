47fd7b9342143dcb454a1ee0d4f82604c703935e
M7ariam <gmariam07a@gmail.com>
2023-03-22 00:49:48 +0100

    mg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: 0
+*/
+int main(void)
+{
+unsigned int i;
+char c[] = "_putchar";
+for (i = 0; i <= 7; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
