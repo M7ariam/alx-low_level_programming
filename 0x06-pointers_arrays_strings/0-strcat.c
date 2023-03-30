f2f65cc89cf6cf0d08c4405d4e393836623b6ae9
M7ariam <gmariam07a@gmail.com>
2023-03-30 01:19:59 +0000

    mg

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,12 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: a pointer to the resulting string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-char *_strcat(char *dest, char *src)
+int _putchar(char c)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
+return (write(1, &c, 1));
 }
