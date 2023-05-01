02ac8d949539b5bc887bd0f80358c80de07d013a
M7ariam <gmariam07a@gmail.com>
2023-05-01 20:10:18 +0100

    g

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,8 +7,8 @@
  *
  * Return: address of the new element, or NULL if it fails
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
+ listint_t *add_nodeint_end(listint_t **head, const int n)
+ {
     listint_t *new_node, *temp;
 
     new_node = malloc(sizeof(listint_t));
