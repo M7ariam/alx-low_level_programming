6d75d7aaea3db9601fa6b15ea63f19b69d42b6f2
M7ariam <gmariam07a@gmail.com>
2023-05-01 23:50:18 +0100

    mg

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,17 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
 	if (head == NULL)
 		return;
 
-	listint_t *current = *head;
-	listint_t *next;
-
-	while (current != NULL)
+	while (*head)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 
 	*head = NULL;
