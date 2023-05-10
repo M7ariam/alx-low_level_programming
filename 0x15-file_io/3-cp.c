7539a0720295ffdaf4953bc388eee335ded53c7e
M7ariam <gmariam07a@gmail.com>
2023-05-10 18:13:58 +0100

    mg

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,33 +12,46 @@ void error_exit(const char *message, int exit_code) {
 
 int main(int argc, char *argv[]) {
     int fd_from, fd_to, bytes_read, bytes_written;
-    char buffer[BUF_SIZE];
 
-    if (argc != 3)
+    if (argc != 3) {
         error_exit("Usage: cp file_from file_to", 97);
-
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-        error_exit("Error: Can't read from file", 98);
-
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    if (fd_to == -1)
-        error_exit("Error: Can't write to file", 99);
-
-    while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0) {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written != bytes_read)
-            error_exit("Error: Can't write to file", 99);
     }
 
-    if (bytes_read == -1)
-        error_exit("Error: Can't read from file", 98);
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
 
-    if (close(fd_from) == -1)
-        error_exit("Error: Can't close fd", 100);
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (fd_to == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+        exit(99);
+    }
 
-    if (close(fd_to) == -1)
-        error_exit("Error: Can't close fd", 100);
+    char buffer[BUF_SIZE];
+    while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0) {
+        bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written == -1 || bytes_written != bytes_read) {
+            dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+            exit(99);
+        }
+    }
+
+    if (bytes_read == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    if (close(fd_from) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        exit(100);
+    }
+
+    if (close(fd_to) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        exit(100);
+    }
 
     return 0;
 }
