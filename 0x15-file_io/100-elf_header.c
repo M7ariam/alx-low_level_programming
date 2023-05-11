158a20e7f0c8145b69d3a6b5bdbcf7a8c907952c
M7ariam <gmariam07a@gmail.com>
2023-05-11 18:04:53 +0100

    mg

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <stdint.h>
+#include <fcntl.h>
+#include <elf.h>
 
-#define EI_NIDENT 16
-
-typedef struct {
-    unsigned char e_ident[EI_NIDENT];
-    uint16_t      e_type;
-    uint16_t      e_machine;
-    uint32_t      e_version;
-    uint64_t      e_entry;
-    uint64_t      e_phoff;
-    uint64_t      e_shoff;
-    uint32_t      e_flags;
-    uint16_t      e_ehsize;
-    uint16_t      e_phentsize;
-    uint16_t      e_phnum;
-    uint16_t      e_shentsize;
-    uint16_t      e_shnum;
-    uint16_t      e_shstrndx;
-} Elf64_Ehdr;
-
-void print_error(const char *message) {
-    fprintf(stderr, "%s\n", message);
+void print_error(char *message)
+{
+    fprintf(stderr, "Error: %s\n", message);
     exit(98);
 }
 
-void print_elf_header(const Elf64_Ehdr *header) {
+void print_elf_header(Elf64_Ehdr *header)
+{
     printf("ELF Header:\n");
     printf("  Magic:   ");
     for (int i = 0; i < EI_NIDENT; i++)
@@ -36,93 +19,101 @@ void print_elf_header(const Elf64_Ehdr *header) {
     printf("\n");
 
     printf("  Class:                             ");
-    switch (header->e_ident[4]) {
-        case 1:
+    switch (header->e_ident[EI_CLASS]) {
+        case ELFCLASS32:
             printf("ELF32\n");
             break;
-        case 2:
+        case ELFCLASS64:
             printf("ELF64\n");
             break;
         default:
-            printf("Unknown\n");
+            printf("Invalid ELF class\n");
             break;
     }
 
     printf("  Data:                              ");
-    switch (header->e_ident[5]) {
-        case 1:
+    switch (header->e_ident[EI_DATA]) {
+        case ELFDATA2LSB:
             printf("2's complement, little endian\n");
             break;
-        case 2:
+        case ELFDATA2MSB:
             printf("2's complement, big endian\n");
+            break;
+        default:
+            printf("Invalid data encoding\n");
+            break;
+    }
+
+    printf("  Version:                           %d (current)\n",
+           header->e_ident[EI_VERSION]);
+
+    printf("  OS/ABI:                            ");
+    switch (header->e_ident[EI_OSABI]) {
+        case ELFOSABI_SYSV:
+            printf("UNIX - System V\n");
+            break;
+        case ELFOSABI_LINUX:
+            printf("UNIX - Linux\n");
+            break;
+        case ELFOSABI_NETBSD:
+            printf("UNIX - NetBSD\n");
             break;
         default:
             printf("Unknown\n");
             break;
     }
 
-    printf("  Version:                           %u (current)\n", header->e_version);
+    printf("  ABI Version:                       %d\n",
+           header->e_ident[EI_ABIVERSION]);
 
-    printf("  OS/ABI:                            ");
-    switch (header->e_ident[7]) {
-        case 0:
-            printf("UNIX - System V\n");
+    printf("  Type:                              ");
+    switch (header->e_type) {
+        case ET_NONE:
+            printf("NONE (No file type)\n");
             break;
-        case 1:
-            printf("HP-UX\n");
+        case ET_REL:
+            printf("REL (Relocatable file)\n");
             break;
-        case 2:
-            printf("NetBSD\n");
+        case ET_EXEC:
+            printf("EXEC (Executable file)\n");
             break;
-        case 3:
-            printf("Linux\n");
+        case ET_DYN:
+            printf("DYN (Shared object file)\n");
             break;
-        case 4:
-            printf("GNU Hurd\n");
-            break;
-        case 6:
-            printf("Solaris\n");
-            break;
-        case 7:
-            printf("AIX\n");
-            break;
-        case 8:
-            printf("IRIX\n");
-            break;
-        case 9:
-            printf("FreeBSD\n");
-            break;
-        case 10:
-            printf("Tru64\n");
-            break;
-        case 11:
-            printf("Novell Modesto\n");
-            break;
-        case 12:
-            printf("OpenBSD\n");
-            break;
-        case 13:
-            printf("OpenVMS\n");
-            break;
-        case 14:
-            printf("NonStop Kernel\n");
-            break;
-        case 15:
-            printf("AROS\n");
-            break;
-        case 16:
-            printf("Fenix OS\n");
-            break;
-        case 17:
-            printf("CloudABI\n");
-            break;
-        case 18:
-            printf("OpenVOS\n");
+        case ET_CORE:
+            printf("CORE (Core file)\n");
             break;
         default:
-            printf("<unknown: %u>\n", header->e_ident[7]);
+            printf("Unknown\n");
             break;
     }
 
-    printf
+    printf("  Entry point address:               0x%lx\n",
+           header->e_entry);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        print_error("Usage: elf_header elf_filename");
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        print_error("Unable to open file");
+    }
+
+    Elf64_Ehdr header;
+    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+        print_error("Unable to read ELF header");
+    }
+
+    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+        header.e_ident[EI_MAG1] != ELFMAG1 ||
+        header.e_ident[EI_MAG2] != ELFMAG2 ||
+        header.e_ident[EI_MAG3] != ELFMAG3) {
+        print_error("File is not an ELF file");
+    }
+
+    print_elf_header(&
 
